6877fc2c602803e569e0f2276d912f48c76ab48a
Harshit Kharayat <harshitkharayat15@gmail.com>
2025-07-10 17:15:08 +0530

    Update utils.js

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 
 
 export const generateToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "2d" });
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
 
 
